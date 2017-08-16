108f86c2a54231e420a2d1857b714667391e0529
savis <savitha.s131@gmail.com>
2017-08-15 20:47:46 -0700

    Added Projects from Udacity cource Deep Learning

---
+++ TVGenerationScript/tv-script-generation/command.sh
@@ -0,0 +1,3 @@
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/v5r9MaJFPcrShzHrKgRTBZ' --NotebookApp.token='' --NotebookApp.allow_origin='*'
---
+++ week1,2/Docs/test.py
@@ -0,0 +1,4 @@
+def test(a,b):
+    print(a+b)
+
+test(10,12)
